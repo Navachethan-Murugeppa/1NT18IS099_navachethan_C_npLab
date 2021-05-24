65522e17e5324407e88c8558c27c97735360f165
Navachethan <mnavachethan12@yahoo.com>
2021-05-24 16:11:12 +0530

    np_lab

--- part-A/socket/tcp/client.c
+++ part-A/socket/tcp/client.c
@@ -10,7 +10,7 @@ void func(int sockfd)
 {
 	char buff[MAX];
 	int n;
-	for (;;) {
+	while(1) {
 		bzero(buff, sizeof(buff));
 		printf("Enter the string : ");
 		n = 0;
@@ -31,8 +31,6 @@ int main()
 {
 	int sockfd, connfd;
 	struct sockaddr_in servaddr, cli;
-
-	// socket create and varification
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1) {
 		printf("socket creation failed...\n");
@@ -42,23 +40,17 @@ int main()
 		printf("Socket successfully created..\n");
 	bzero(&servaddr, sizeof(servaddr));
 
-	// assign IP, PORT
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	servaddr.sin_port = htons(PORT);
 
-	// connect the client socket to server socket
 	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
 		printf("connection with the server failed...\n");
 		exit(0);
 	}
 	else
-		printf("connected to the server..\n");
-
-	// function for chat
+		printf("Enter data to transfer:\n");
 	func(sockfd);
-
-	// close the socket
 	close(sockfd);
 }
 
